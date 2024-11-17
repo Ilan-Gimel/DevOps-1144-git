72d44cfaa70230f788e9495cb5cddf2af403bc7d
Ilan-Gimel <ilanorilan123@gmail.com>
2024-11-17 14:50:17 +0200

    Python File Handling

---
+++ Python/DevOps-Py-4.py
@@ -0,0 +1,30 @@
+#                    Basic File Operations with Date Logging                #
+
+
+#Calculator
+# def calc(num1:int,operation:str,num2:int):
+#     print('-'*24)
+#     for i in range(10):
+#         if i==1:
+#             print('|',num1,operation,num2,'=',(f'\033[31m{eval(f'{num1} {operation} {num2}')}\033[0m'))
+#             print('|'+'-'*22+'|')
+#         if i==3:
+#             print('| ',7,'  ',8,'  ',9,'  ','x','   |')
+#         if i==5:
+#             print('| ',4,'  ',5,'  ',6,'  ','-','   |')
+#         if i==7:
+#             print('| ',1,'  ',2,'  ',3,'  ','+','   |')
+#         if i==9:
+#             print('| ',0,'   ','-','    ','=','     |')
+#         print('|',' '*16,'    |')
+#     print('-'*24)
+# num1=int(input("Enter First Number : "))
+# operation=str(input("Enter Operator : "))
+# num2=int(input("Enter Second Number : "))
+# calc(num1,operation,num2)
+
+
+
+
+
+
