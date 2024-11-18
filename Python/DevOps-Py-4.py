6db822e99b6f479962c9d697a0e28b5dd2c1bde3
Ilan-Gimel <ilanorilan123@gmail.com>
2024-11-18 11:30:45 +0200

    Python File Handling Update

--- Python/DevOps-Py-4.py
+++ Python/DevOps-Py-4.py
@@ -23,6 +23,49 @@
 # num2=int(input("Enter Second Number : "))
 # calc(num1,operation,num2)
 
+#Ex 1
+import datetime
+import os
+date1=datetime.datetime.now()
+date2=date1.strftime("%Y-%m-%d %H:%M:%S")
+if os.path.exists("./DevOps-1144-git/Python/Exer1.txt")==False:
+    file=open("./DevOps-1144-git/Python/Exer1.txt","x")
+write=str(input('Enter Content : '))
+while 'done' not in write:
+    file=open("./DevOps-1144-git/Python/Exer1.txt","a")
+    file.write(str(date2)+' '+write+"\n")
+    file.close()
+    write=str(input('Enter Content to Stop type "done" :  '))
+
+file=open("./DevOps-1144-git/Python/Exer1.txt","r")
+print(file.read())
+file.close()
+
+#Ex 2
+# file=open("./DevOps-1144-git/Python/Exer1.txt","r")
+# for line in file:
+#     print(line)
+# file.close()
+
+#Ex 3
+# more=str(input("Would you like to add more enteries ?? (yes/no)"))
+# while 'no' not in more:
+#     file=open("./DevOps-1144-git/Python/Exer1.txt","a")
+#     write=str(input('Enter Content : '))
+#     file.write("\n"+' '+write+str(date2))
+#     more=str(input("Would you like to add more enteries ?? (yes/no)"))
+# file.close()
+
+#Ex 4
+file=open("./DevOps-1144-git/Python/Exer1.txt","r")
+count_lines=0
+count_words=-1
+for lines in file:
+    count_lines+=1
+    for words in lines[20:].split(" "):
+        count_words+=1
+print("Number Of Enteries : ",count_lines,"Number Of Words : ",count_words )
+file.close()
 
 
 
